bfc582ddb859a9207c629b13ef13920bf7cd4110
Alex Wexouv <wexouv@gmail.com>
2021-11-08 19:50:56 +0500

    Added models

---
+++ App/Models/UserModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace App.Models
+{
+    public class User : IdentityUser
+    {
+
+        public virtual ICollection<Question> Questions { get; set; }
+    }
+}
