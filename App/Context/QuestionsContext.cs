fc9ae0d2759bd3e01429b93012f3faee8dedfebd
Alex Wexouv <wexouv@gmail.com>
2021-11-08 19:50:38 +0500

    Added contexts

---
+++ App/Context/QuestionsContext.cs
@@ -0,0 +1,29 @@
+using App.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviePortal.Context
+{
+    public class QuestionsContext : IdentityDbContext<User>
+    {
+        public QuestionsContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(entity => entity.ToTable("Users"));
+            builder.Entity<IdentityRole>(entity => entity.ToTable("Roles"));
+            builder.Entity<IdentityUserRole<string>>(entity => entity.ToTable("UserRoles"));
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.ToTable("UserClaims"));
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.ToTable("UserLogins"));
+            builder.Entity<IdentityUserToken<string>>(entity => entity.ToTable("UserTokens"));
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.ToTable("RoleClaims"));
+        }
+    }
+}
