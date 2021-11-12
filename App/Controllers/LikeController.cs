ddfecb898ecd3b67b6214bc79df409b8b5860bde
Shekhov Ismoil <veyxov@gmail.com>
2021-11-12 22:59:52 +0500

    Fix: Liking counter

--- App/Controllers/LikeController.cs
+++ App/Controllers/LikeController.cs
@@ -20,22 +20,38 @@ namespace App.Controllers
 
         public async Task<IActionResult> IncrementAsync(Guid id)
         {
-            var question = await _db.Questions.FindAsync(id);
-            // Increment the likes
-            question.Likes += 1;
-
-            await _db.SaveChangesAsync();
-            return RedirectToAction("Show", "Question", new { Id = id });
+            try {
+                var question = await _db.Questions.FindAsync(id);
+                // Increment the likes
+                question.Likes += 1;
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Show", "Question", new { Id = id });
+            } catch {
+                var question = await _db.Answers.FindAsync(id);
+                // Increment the likes
+                question.Likes += 1;
+                return RedirectToAction("Show", "Question", new { Id = question.QuestionId });
+            } finally {
+                await _db.SaveChangesAsync();
+            }
         }
 
         public async Task<IActionResult> DecrementAsync(Guid id)
         {
-            var question = await _db.Questions.FindAsync(id);
-            // Decrement the likes
-            question.Likes -= 1;
-
-            await _db.SaveChangesAsync();
-            return RedirectToAction("Show", "Question", new { Id = id });
+            try {
+                var question = await _db.Questions.FindAsync(id);
+                // Increment the likes
+                question.Likes -= 1;
+                return RedirectToAction("Show", "Question", new { Id = id });
+            } catch {
+                var question = await _db.Answers.FindAsync(id);
+                // Increment the likes
+                question.Likes -= 1;
+                return RedirectToAction("Show", "Question", new { Id = question.QuestionId });
+            }
+            finally {
+                await _db.SaveChangesAsync();
+            }
         }
     }
 }
