face67dd62e875c834540860303077c84f51f0a3
Alex Wexouv <wexouv@gmail.com>
2021-11-12 16:42:57 +0500

    Feature: Showing questions on home screen

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -6,21 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using App.Models;
+using MoviePortal.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly QuestionsContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, QuestionsContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Questions.ToListAsync().Result);
         }
 
         public IActionResult Privacy()
