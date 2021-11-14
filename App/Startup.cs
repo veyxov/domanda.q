a4caf82fb1f43194cc30a237f136fc608ac2a94c
Shekhov Ismoil <veyxov@gmail.com>
2021-11-14 09:11:52 +0500

    Added lazy loading

--- App/Startup.cs
+++ App/Startup.cs
@@ -1,12 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,8 +26,10 @@ namespace App
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<QuestionsContext>(options =>
-                options.UseSqlite( Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<QuestionsContext>(options => 
+                        options.UseLazyLoadingProxies()
+                               .UseSqlite( Configuration.GetConnectionString("DefaultConnection"))
+                    );
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
