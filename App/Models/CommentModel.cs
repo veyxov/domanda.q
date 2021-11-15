419e8a3ea2a0c4d37f9d2b069af7453802148765
Shekhov Ismoil <veyxov@gmail.com>
2021-11-15 21:48:14 +0500

    Fix: Fixed comments for answers

--- App/Models/CommentModel.cs
+++ App/Models/CommentModel.cs
@@ -17,11 +17,11 @@ namespace App.Models
 
         public DateTime CreationDate { get; set; }
 
-        public Guid? QuestionId { get; set; }
+        public Nullable<Guid> QuestionId { get; set; }
         [ForeignKey("QuestionId")]
         public virtual Question Question { get; set; }
 
-        public Guid? AnswerId { get; set; }
+        public Nullable<Guid> AnswerId { get; set; }
         [ForeignKey("AnswerId")]
         public virtual Answer Answer  { get; set; }
     }
