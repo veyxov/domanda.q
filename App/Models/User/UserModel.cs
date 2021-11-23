6fff02e3d1a2f455dc93882ebd6269036eaca441
Shekhov Ismoil <veyxov@gmail.com>
2021-11-23 21:01:35 +0500

    Added Profile showing view

--- App/Models/User/UserModel.cs
+++ App/Models/User/UserModel.cs
@@ -12,6 +12,7 @@ namespace App.Models
 
         public virtual ICollection<Question> Questions { get; set; }
         public virtual ICollection<Answer> Answers { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<LikedPost> LikedPosts { get; set; }
     }
 }
